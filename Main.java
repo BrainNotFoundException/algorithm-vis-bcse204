6939629532aad20003c84899d1736911b9b502db
Lavnish <lavjhn@gmail.com>
2026-04-12 20:20:45 +0530

    add gap between algorithm windows and temporarily reduce array size

--- Main.java
+++ Main.java
@@ -12,10 +12,10 @@ public class Main {
     public static void main(String[] args) {
 
         JFrame frame = new JFrame("Sorting Algorithm Visualizer");
-        frame.setLayout(new GridLayout(1, 3));
+        frame.setLayout(new GridLayout(1, 3, 10, 10));
         frame.setMinimumSize(new Dimension(1800, 900));
 
-        int[] arr = ArrGen.generateArray(1000);
+        int[] arr = ArrGen.generateArray(100);
         BubbleSortPanel bpanel = new BubbleSortPanel(arr.clone());
         frame.add(bpanel);
         MergeSortPanel mpanel = new MergeSortPanel(arr.clone());
