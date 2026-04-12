f72a99b90f2a0a094d0823db16df31a50d75799e
Lavnish <lavjhn@gmail.com>
2026-04-12 20:48:57 +0530

    add themes, and start sorting after all UI elements have been loaded.

--- Main.java
+++ Main.java
@@ -1,7 +1,12 @@
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.UIManager;
+import themes.Theme;
 import utils.ArrGen;
 import utils.BubbleSortPanel;
 import utils.MergeSortPanel;
@@ -12,24 +17,37 @@ public class Main {
     public static void main(String[] args) {
 
         JFrame frame = new JFrame("Sorting Algorithm Visualizer");
-        frame.setLayout(new GridLayout(1, 3, 10, 10));
+
+        UIManager.put("Panel.background", Theme.BG_Color);
+        UIManager.put("Button.background", Theme.Button_Bg);
+        UIManager.put("Button.foreground", Theme.Button_Fg);
+
+        frame.setLayout(new BorderLayout());
+        // frame.setLayout(new GridLayout(1, 3, 10, 10));
         frame.setMinimumSize(new Dimension(1800, 900));
+
+        JPanel container = new JPanel(new GridLayout(1, 3, 20, 20));
+        container.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+        container.setBackground(Theme.BG_Color);
 
         int[] arr = ArrGen.generateArray(100);
         BubbleSortPanel bpanel = new BubbleSortPanel(arr.clone());
-        frame.add(bpanel);
         MergeSortPanel mpanel = new MergeSortPanel(arr.clone());
-        frame.add(mpanel);
         QuickSortPanel qpanel = new QuickSortPanel(arr.clone());
-        frame.add(qpanel);
 
-        bpanel.startSorting();
-        mpanel.startSorting();
-        qpanel.startSorting();
+        container.add(bpanel);
+        container.add(mpanel);
+        container.add(qpanel);
+
+        frame.add(container, BorderLayout.CENTER);
 
         frame.pack();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+
+        bpanel.startSorting();
+        mpanel.startSorting();
+        qpanel.startSorting();
     }
 
 }
---
+++ themes/Theme.java
@@ -0,0 +1,11 @@
+package themes;
+
+import java.awt.Color;
+
+public class Theme {
+    
+    public static final Color BG_Color = new Color(18, 18, 18);
+    public static final Color Button_Bg = new Color(50, 50, 50);
+    public static final Color Button_Fg = Color.WHITE;
+
+}
