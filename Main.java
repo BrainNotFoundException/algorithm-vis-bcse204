2fb58091a5084207eb5480dc8d3bb9e622948e69
Lavnish <lavjhn@gmail.com>
2026-04-12 21:16:44 +0530

    better ui using card layout with labelled headings for each sorting algorithm.

--- Main.java
+++ Main.java
@@ -1,16 +1,8 @@
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.GridLayout;
-import javax.swing.BorderFactory;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.UIManager;
+import java.awt.*;
+import javax.swing.*;
 import themes.Theme;
-import utils.ArrGen;
-import utils.BubbleSortPanel;
-import utils.MergeSortPanel;
-import utils.QuickSortPanel;
+import utils.*;
 
 public class Main {
 
@@ -18,9 +10,9 @@ public class Main {
 
         JFrame frame = new JFrame("Sorting Algorithm Visualizer");
 
-        UIManager.put("Panel.background", Theme.BG_Color);
-        UIManager.put("Button.background", Theme.Button_Bg);
-        UIManager.put("Button.foreground", Theme.Button_Fg);
+        UIManager.put("Panel.background", Theme.BG_COLOR);
+        UIManager.put("Button.background", Theme.BUTTON_BG);
+        UIManager.put("Button.foreground", Theme.BUTTON_FG);
 
         frame.setLayout(new BorderLayout());
         // frame.setLayout(new GridLayout(1, 3, 10, 10));
@@ -28,16 +20,16 @@ public class Main {
 
         JPanel container = new JPanel(new GridLayout(1, 3, 20, 20));
         container.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
-        container.setBackground(Theme.BG_Color);
+        container.setBackground(Theme.BG_COLOR);
 
         int[] arr = ArrGen.generateArray(100);
         BubbleSortPanel bpanel = new BubbleSortPanel(arr.clone());
         MergeSortPanel mpanel = new MergeSortPanel(arr.clone());
         QuickSortPanel qpanel = new QuickSortPanel(arr.clone());
 
-        container.add(bpanel);
-        container.add(mpanel);
-        container.add(qpanel);
+        container.add(createCard("Bubble Sort", bpanel));
+        container.add(createCard("Merge Sort", mpanel));
+        container.add(createCard("Quick Sort", qpanel));
 
         frame.add(container, BorderLayout.CENTER);
 
@@ -48,6 +40,26 @@ public class Main {
         bpanel.startSorting();
         mpanel.startSorting();
         qpanel.startSorting();
+    }
+
+    public static JPanel createCard(String title, JPanel panel) {
+
+        JPanel card = new JPanel(new BorderLayout());
+        card.setBackground(new Color(30, 30, 30));
+
+        card.setBorder(BorderFactory.createCompoundBorder(
+                BorderFactory.createLineBorder(Theme.BORDER_COLOR, 1),
+                BorderFactory.createEmptyBorder(10, 10, 10, 10)
+        ));
+
+        JLabel label = new JLabel(title, SwingConstants.CENTER);
+        label.setForeground(Theme.BUTTON_FG);
+        label.setFont(new Font("JetBrainsMono NFP", Font.BOLD, 16));
+
+        card.add(label, BorderLayout.NORTH);
+        card.add(panel, BorderLayout.CENTER);
+
+        return card;
     }
 
 }
--- themes/Theme.java
+++ themes/Theme.java
@@ -4,8 +4,9 @@ import java.awt.Color;
 
 public class Theme {
     
-    public static final Color BG_Color = new Color(18, 18, 18);
-    public static final Color Button_Bg = new Color(50, 50, 50);
-    public static final Color Button_Fg = Color.WHITE;
+    public static final Color BG_COLOR = new Color(18, 18, 18);
+    public static final Color BUTTON_BG = new Color(50, 50, 50);
+    public static final Color BUTTON_FG = Color.WHITE;
+    public static final Color BORDER_COLOR = Color.DARK_GRAY;
 
 }
