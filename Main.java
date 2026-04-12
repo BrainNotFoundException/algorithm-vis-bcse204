bbd7da8e34df3062a3405aa713455f00a2f264d6
Lavnish <lavjhn@gmail.com>
2026-04-12 19:03:19 +0530

    add the panels to window and increase window size

--- Main.java
+++ Main.java
@@ -1,7 +1,11 @@
+
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import javax.swing.JFrame;
 import utils.ArrGen;
+import utils.BubbleSortPanel;
+import utils.MergeSortPanel;
+import utils.QuickSortPanel;
 
 public class Main {
 
@@ -9,9 +13,12 @@ public class Main {
 
         JFrame frame = new JFrame("Sorting Algorithm Visualizer");
         frame.setLayout(new GridLayout(1, 3));
-        frame.setMinimumSize(new Dimension(600, 300));
+        frame.setMinimumSize(new Dimension(1800, 900));
 
         int[] arr = ArrGen.generateArray(100);
+        frame.add(new BubbleSortPanel(arr));
+        frame.add(new MergeSortPanel(arr));
+        frame.add(new QuickSortPanel(arr));
 
         frame.pack();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
