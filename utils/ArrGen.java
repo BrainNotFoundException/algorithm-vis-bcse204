5c9df5331dd3ce1958bc7f17c1fdd1b67bcf9541
Lavnish <lavjhn@gmail.com>
2026-04-13 17:49:09 +0530

    reformat ArrGen and add option to set lower and upper bound on array generated

--- utils/ArrGen.java
+++ utils/ArrGen.java
@@ -3,12 +3,13 @@ package utils;
 import java.util.Random;
 
 public interface ArrGen {
-    public static int[] generateArray(int size){
+
+    public static int[] generateArray(int size, int min, int max) {
         int[] arr = new int[size];
         Random rand = new Random();
 
-        for(int i=0; i<size; i++){
-            arr[i] = rand.nextInt(1000)+1;
+        for (int i = 0; i < size; i++) {
+            arr[i] = rand.nextInt(max - min) + min + 1;
         }
 
         return arr;
